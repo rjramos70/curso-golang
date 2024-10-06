7ed9ee106cc6e3217b025809d987fcde8d7e1348
rjramos70 <rjramos70@gmail.com>
2024-10-06 21:53:06 +0100

    Primeira aula de fundamentos

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,23 @@
+// Programas executaveis iniciam pelo pacote main
+package main
+
+/*
+Os códigos em Go são organizados em pacotes
+e para usá-los é necessário declarar um ou vários imports
+*/
+import "fmt"
+
+// A função main é o entrypoint de um programa em Go
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa")
+
+	/*
+		Sobre comentários...
+
+		1) Priorize código legível e só faça comentários que agregem valor!
+		2) Evite comentário óbvios
+		3) Durante o curso abuse dos comentários
+	*/
+
+}
